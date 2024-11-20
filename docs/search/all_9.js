84e723ef7898024f4c950327b87fb22485b0b10b
tony-josi-aws <tony-josi-aws@users.noreply.github.com>
2024-11-20 11:02:06 +0000

    Auto-generate Doxygen documentation via Github Actions

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -5,7 +5,7 @@ var searchData=
   ['jobengine_159',['JobEngine',['../class_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_job_engine.html',1,'Aws::Iot::DeviceClient::Jobs']]],
   ['jobexecutionstatusinfo_160',['JobExecutionStatusInfo',['../struct_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_jobs_feature_1_1_job_execution_status_info.html',1,'Aws::Iot::DeviceClient::Jobs::JobsFeature']]],
   ['jobhandlerdir_161',['jobHandlerDir',['../class_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_jobs_feature.html#a37030f5e63e8d42448dab3a0dd70d7b2',1,'Aws::Iot::DeviceClient::Jobs::JobsFeature']]],
-  ['jobs_162',['Jobs',['../struct_aws_1_1_iot_1_1_device_client_1_1_plain_config_1_1_jobs.html',1,'Aws::Iot::DeviceClient::PlainConfig::Jobs'],['../md__home_runner_work_aws_iot_device_client_aws_iot_device_client_source_jobs__r_e_a_d_m_e.html',1,'(Global Namespace)']]],
+  ['jobs_162',['Jobs',['../struct_aws_1_1_iot_1_1_device_client_1_1_plain_config_1_1_jobs.html',1,'Aws::Iot::DeviceClient::PlainConfig::Jobs'],['../md__home_runner_work_aws_iot_device_client_aws_iot_device_client_doxygen_store_source_jobs__r_e_a_d_m_e.html',1,'(Global Namespace)']]],
   ['jobsclient_163',['jobsClient',['../class_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_jobs_feature.html#a0213a095d51d9e6d64a4ae467043d17a',1,'Aws::Iot::DeviceClient::Jobs::JobsFeature']]],
   ['jobsfeature_164',['JobsFeature',['../class_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_jobs_feature.html',1,'Aws::Iot::DeviceClient::Jobs']]]
 ];
--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['named_20shadows_238',['Named Shadows',['../md__home_runner_work_aws_iot_device_client_aws_iot_device_client_source_shadow__r_e_a_d_m_e.html',1,'']]],
+  ['named_20shadows_238',['Named Shadows',['../md__home_runner_work_aws_iot_device_client_aws_iot_device_client_doxygen_store_source_shadow__r_e_a_d_m_e.html',1,'']]],
   ['needpublish_239',['needPublish',['../class_aws_1_1_iot_1_1_device_client_1_1_sensor_publish_1_1_sensor.html#abd9aa91b82f595d46c8a7012049f5bda',1,'Aws::Iot::DeviceClient::SensorPublish::Sensor']]],
   ['needsshutdown_240',['needsShutdown',['../class_aws_1_1_iot_1_1_device_client_1_1_logging_1_1_file_logger.html#a0e7a54d7e865f03ba082821248e4beef',1,'Aws::Iot::DeviceClient::Logging::FileLogger::needsShutdown()'],['../class_aws_1_1_iot_1_1_device_client_1_1_logging_1_1_std_out_logger.html#a4e526296245920334c21e20e5cad0d75',1,'Aws::Iot::DeviceClient::Logging::StdOutLogger::needsShutdown()']]],
   ['needstop_241',['needStop',['../class_aws_1_1_iot_1_1_device_client_1_1_jobs_1_1_jobs_feature.html#a155d798a5de563ba398e8d2a9ec8594c',1,'Aws::Iot::DeviceClient::Jobs::JobsFeature::needStop()'],['../class_aws_1_1_iot_1_1_device_client_1_1_samples_1_1_pub_sub_feature.html#a5d584f705f437f295948c3134e3eb9a4',1,'Aws::Iot::DeviceClient::Samples::PubSubFeature::needStop()'],['../class_aws_1_1_iot_1_1_device_client_1_1_shadow_1_1_sample_shadow_feature.html#ac61c70f00de73f8e997ef73780224570',1,'Aws::Iot::DeviceClient::Shadow::SampleShadowFeature::needStop()']]],
